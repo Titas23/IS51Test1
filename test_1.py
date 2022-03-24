c745ea7646cb77554d58e28a146edf5a488611d9
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-03-24 15:20:48 -0700

    structured-english done

---
+++ test_1.py
@@ -0,0 +1,13 @@
+"""
+We are going to determine whether if
+option_1: Pays $100 per day 
+or 
+option_2: pays $1 first day then it doubles everyday for ten days
+is better 
+if option_1 pays more then display: Option 1 is better 
+else 
+if option_2 is pays more then display: Option 2 is better
+if both options equal the same amount then both 
+we will display option_1 and option_2 pay the same amount. 
+
+"""
