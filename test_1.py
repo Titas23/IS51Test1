f811e2bc20879d25024878c5d7592a784fdd1f34
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-03-24 15:29:25 -0700

    pseudo-code done

--- test_1.py
+++ test_1.py
@@ -11,3 +11,25 @@ if both options equal the same amount then both
 we will display option_1 and option_2 pay the same amount. 
 
 """
+
+"""
+option_1 = 
+    100 * 10 
+
+option_2 = 
+    amount = 1
+    days = []
+    loop the amount 10 times 
+    times the amount by 2
+total = sum(days)
+return the total amount
+
+result
+    if option_1 == option_2
+        display both options pay the same amount
+    if option_1 > option_2 
+        display option_1 is better 
+    if option 2 > option_1 
+        display option_2 is better
+    
+"""
